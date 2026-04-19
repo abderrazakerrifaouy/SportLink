6f90719899f5708fb3a0cb8fa36747d58fb60c8c
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-04-19 12:46:31 +0100

    fixed club admine services vue

--- frontEnd/app/src/services/cloubAdminService.js
+++ frontEnd/app/src/services/cloubAdminService.js
@@ -1,7 +1,7 @@
 import api from "@/api/api";
 
 export default {
-  
+
   getClubAdmins: () => apiClient.get('/club-admins'),
 
   createClubAdmin: (data) => apiClient.post('/club-admins', data),
@@ -15,8 +15,6 @@ export default {
   searchByName: (name) => apiClient.get(`/club-admins/search/${name}`),
 
   getTitresByClubAdminId: (clubAdminId) => apiClient.get(`/club-admins/${clubAdminId}/titres`),
-
-  showTitre: (titreId) => apiClient.get(`/titres/${titreId}`),
 
   createTitre: (data) => apiClient.post('/titres', data),
 
